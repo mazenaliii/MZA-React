0f3754b45de743ebf10ff544163efd3b648a8e71
mazenaliii <mazenpro069@gmail.com>
2023-10-08 00:29:56 +0200

    something

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import styles from './page.module.css'
-import logo from '/MZA_Logo_No_Text_-removebg-preview.png'
+
 
 
 export const metadata = {
   title: 'MZA',
   description: 'MZA is a full digital company that offers everything belongs to web development services',
-  image: logo,
 }
 
 export default function RootLayout({
